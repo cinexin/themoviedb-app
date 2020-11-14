258e4d5effbe205f0b9015e22e2e12300e38b705
cinexin <migueguillamon@gmail.com>
2020-11-14 13:49:14 +0100

    base routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MoviesComponent } from './pages/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'movie/:id',
+    component: MoviesComponent
+  },
+  {
+    path: 'search/:text',
+    component: SearchComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/home  '
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
