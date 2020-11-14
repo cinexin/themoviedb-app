4d336bcda05c054af199ad41bb278123cf4d4cc7
cinexin <migueguillamon@gmail.com>
2020-11-14 14:12:28 +0100

    Navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ComponentsModule } from './components/components.module';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
