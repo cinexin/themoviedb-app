b5d6e40f97ead27046778d2dd65d686bcf3176e6
cinexin <migueguillamon@gmail.com>
2020-11-15 15:06:04 +0100

    Infinite scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Movie, NowPlayingResponse } from '../../interfaces/now-playing-response';
 import { MoviesService } from '../../services/movies.service';
 
@@ -10,15 +10,32 @@ import { MoviesService } from '../../services/movies.service';
 export class HomeComponent implements OnInit {
 
   movies: Movie[] = [];
+  moviesSlideShow: Movie[] = [];
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    const threshold = 1300;
+    const position = (document.documentElement.scrollTop || document.body.scrollTop) + threshold;
+    const maxPosition = document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    if (position > maxPosition) {
+      if (!this.moviesService.loading && !this.moviesService.fullyLoaded) {
+        this.moviesService.getNowPlaying().subscribe((response: Movie[]) => {
+          this.movies.push(...response);
+        });
+      }
+    }
+  }
 
   constructor(
     private moviesService: MoviesService
   ) { }
 
   ngOnInit(): void {
-    this.moviesService.getNowPlaying().subscribe((data: NowPlayingResponse) => {
+    this.moviesService.getNowPlaying().subscribe((data: Movie[]) => {
       console.log(data);
-      this.movies = data.results;
+      this.movies = data;
+      this.moviesSlideShow = data;
     },
     (error) => {
       console.log(error);
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,19 +1,57 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { NowPlayingResponse } from '../interfaces/now-playing-response';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { Movie, NowPlayingResponse } from '../interfaces/now-playing-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
+  private baseUrl = 'https://api.themoviedb.org/3';
+  private apiKey = '2efa553fe649d30ce61733c0767e9ac3';
+  private lang = 'es-ES';
+  private region = 'ES';
+  private page = 1;
+  private totalPages = 1;
+  public fullyLoaded = false;
+  public loading = false;
+
   constructor(
     private http: HttpClient,
   ) { }
 
-  getNowPlaying(): Observable<NowPlayingResponse> {
-    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=2efa553fe649d30ce61733c0767e9ac3&language=es-ES&region=ES`;
-    return this.http.get<NowPlayingResponse>(url);
+  get params(): any {
+    return {
+      api_key: this.apiKey,
+      language: this.lang,
+      region: this.region,
+      page: this.page,
+    };
+  }
+
+  getNowPlaying(): Observable<Movie[]> {
+    if (this.loading) {
+      return of([]);
+    }
+
+    const url = `${this.baseUrl}/movie/now_playing`;
+    console.log('Calling API...');
+    this.loading = true;
+    return this.http.get<NowPlayingResponse>(url, {params: this.params})
+    .pipe(
+      tap(() => {
+        this.page += 1;
+        if (this.page === this.totalPages) {
+          this.fullyLoaded = true;
+        }
+        this.loading = false;
+      }),
+      map( response => {
+        this.totalPages = response.total_pages;
+        return response.results;
+      })
+    );
   }
 }
