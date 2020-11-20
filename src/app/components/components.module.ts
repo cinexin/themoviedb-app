b67fe8852fed6ccba6ace01367d9fd9e1c3e9267
cinexin <migueguillamon@gmail.com>
2020-11-20 18:46:41 +0100

    Cast

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,11 +6,12 @@ import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MoviesPosterGridComponent } from './movies-poster-grid/movies-poster-grid.component';
 import { RatingModule } from 'ng-starrating';
 import { PipesModule } from '../pipes/pipes.module';
+import { CastSlideshowComponent } from './cast-slideshow/cast-slideshow.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, SlideshowComponent, MoviesPosterGridComponent],
+  declarations: [NavbarComponent, SlideshowComponent, MoviesPosterGridComponent, CastSlideshowComponent],
   imports: [
     CommonModule,
     RouterModule,
@@ -21,6 +22,7 @@ import { PipesModule } from '../pipes/pipes.module';
     NavbarComponent,
     SlideshowComponent,
     MoviesPosterGridComponent,
+    CastSlideshowComponent,
   ]
 })
 export class ComponentsModule { }
